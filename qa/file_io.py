43da524d639a771e8c5fade69ce734245253f430
sriyavasudevan <developments.sriya@gmail.com>
2022-04-23 00:53:27 -0400

    added new method to directly feed questions, measure time taken and more documentation

---
+++ qa/file_io.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def read_data(filename):
+    """
+    Helper method to read in json file and convert into dataframe
+    :param filename: name of file
+    :return: dataframe
+    """
+    sample_text = pd.read_json(filename)
+    return sample_text
+
+
+def write_data(conversation_df, first_time=False):
+    """
+    Helper method to write dataframe into a csv
+    :param conversation_df: dataframe to be converted
+    :param first_time: check if its the first time this method is called
+    :return: nothing
+    """
+    filename = 'conversation_log_to_show.csv'
+    """if os.path.isfile(filename):
+        os.remove(filename)"""
+    if first_time:
+        conversation_df.to_csv(filename, mode='a')
+    else:
+        conversation_df.to_csv(filename, mode='a', header=False)
+
--- qa/qa_bert.py
+++ qa/qa_bert.py
@@ -3,19 +3,31 @@ import numpy as np
 import torch
 from transformers import BertForQuestionAnswering
 from transformers import BertTokenizer
+from time import time
+import platform
+import file_io
 
 
-def read_data(filename):
+def get_time(fun, *args, **kwargs):
     """
-    This method is for reading in the data
+    Gets the runtime of a method
+    :param fun: name of the method
+    :param args: variable arguments
+    :param kwargs: variable arguments
+    :return: time in seconds
     """
-    sample_text = pd.read_json(filename)
-    return sample_text
+    start = time()
+    res = fun(*args, **kwargs)
+    return time() - start, res
 
 
 def question_answer(question, context, question_history):
     """
-    This is the main code for qa system
+    This is the main code for qa system - uses BERT
+    :param question: current question being asked
+    :param context: current context used
+    :param question_history: list of questions previously asked
+    :return: current context and the predicted answer
     """
     inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
     input_ids = inputs["input_ids"].tolist()[0]
@@ -43,21 +55,64 @@ def question_answer(question, context, question_history):
     print("\nPredicted answer:\n{}".format(answer.capitalize()))
 
     context = ''.join(question_history) + context
-    # print("----")
-    # print("The current context is after question: " + context)
+    # print("Current context: " + context)
+    
+    return context, answer
 
 
-def begin_conversation(inital_context):
-    '''
-    This method output instructions to begin the conversation
-    '''
+def test_conversation(initial_context, question_list):
+    """
+    Can use this method to directly pass in a list of questions instead of typing it in
+    :param initial_context: the initial piece of context
+    :param question_list: list of questions you want to ask
+    :return: nothing
+    """
     question_history = []
+    logging_list =[]
+    text = initial_context
+
+    for i, question in enumerate(question_list):
+        """if question + " " not in question_history:
+            question_history.append(question + " ")"""
+        question_history.append(question + " ")
+        time_taken_to_answer, qa_returned_elements = get_time(question_answer, question, text, question_history)
+        current_answer = qa_returned_elements[1]
+
+        # logging
+        logging_list.append([question, ''.join(question_history), current_answer, str(time_taken_to_answer)])
+        logging_df = pd.DataFrame(logging_list, columns=['Current Question', 'Question History', 'Predicted Answer', 'Time taken'])
+        if i == 0:
+            file_io.write_data(logging_df, first_time=True)
+        else:
+            file_io.write_data(logging_df)
+        logging_list = []
+
+
+def begin_conversation(initial_context):
+    """
+    This method output instructions to begin the conversation
+    :param initial_context: the inital piece of text
+    :return: nothing
+    """
+    question_history = []
+    # logging_list =[]
+
     question = input("\nPlease enter your question: \n")
-    question_history.append(question)
-    text = inital_context
+    question_history.append(question + " ")
+    text = initial_context
 
     while True:
-        question_answer(question, text, question_history)
+        # checking the time taken by each call
+        time_taken_to_answer, qa_returned_elements = get_time(question_answer, question, text, question_history)
+        print("Time taken: " + str(time_taken_to_answer))
+        # current_context = qa_returned_elements[0]
+        current_answer = qa_returned_elements[1]
+
+        """logging
+        logging_list.append([question, ''.join(question_history), current_answer, str(time_taken_to_answer)])
+        logging_df = pd.DataFrame(logging_list, columns=['Current Question', 'Question History', 'Predicted Answer', 'Time taken'])
+        file_io.write_data(logging_df)
+        logging_list = []"""
 
         flag = True
         flag_N = False
@@ -66,12 +121,11 @@ def begin_conversation(inital_context):
             response = input("\nDo you want to ask another question based on this text (Y/N)? ")
             if response[0] == "Y":
                 question = input("\nPlease enter your question: \n")
-                question_history.append(question)
-
+                question_history.append(question + " ")
                 flag = False
+
             elif response[0] == "N":
                 print("\nBye!")
-                question_history = []
                 flag = False
                 flag_N = True
 
@@ -79,15 +133,27 @@ def begin_conversation(inital_context):
             break
 
 
+# initialize the model and the tokenizer
 model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
 tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
 
-index = 0
-inital_text = read_data('sample_dataset.json')["text"][index]
-print("The initial context: " + inital_text)
+# pick the context from dataset
+index = 10
+initial_text = file_io.read_data('sample_dataset.json')["text"][index]
 
-begin_conversation(inital_text)
+# checking the current environment type
+print("Current environment: ")
+print(platform.uname())
+
+# print the initial context and start conversation
+print("The initial context: " + initial_text)
+begin_conversation(initial_text)
 
 """
-push to development
-"""
+# use this block of code to be able to feed questions directly instead of typing each time
+# data file contains text and list of questions
+test_df = file_io.read_data('test_file_to_show.json')
+for i in range(0, test_df.shape[0]):
+    initial_context = test_df["data"][i]["text"]
+    q_list = test_df["data"][i]["questions"]
+    test_conversation(initial_context, q_list)"""
